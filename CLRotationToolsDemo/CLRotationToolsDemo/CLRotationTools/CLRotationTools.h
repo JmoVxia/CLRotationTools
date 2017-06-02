be268adcbd15e4847678095177c99b804ee69624
JmoVxia <269968846@qq.com>
2017-06-02 13:24:06 +0800

    增加注释

--- CLRotationToolsDemo/CLRotationToolsDemo/CLRotationTools/CLRotationTools.h
+++ CLRotationToolsDemo/CLRotationToolsDemo/CLRotationTools/CLRotationTools.h
@@ -9,5 +9,12 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 @interface CLRotationTools : NSObject
+
+/**
+ 需要支持旋转的控制器
+
+ @param needRotationViewController 控制器
+ */
 + (void)needRotationViewController:(UIViewController *)needRotationViewController;
+
 @end
